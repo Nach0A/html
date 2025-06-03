667153a7c5cd48550c6ab76b69d896720f648e02
JuAND547 <juanbau.rod2007@gmail.com>
2025-06-03 09:22:00 -0300

    Add files via upload

--- conexion_BD/conexion_BD.php
+++ conexion_BD/conexion_BD.php
@@ -3,15 +3,17 @@ $servidor = "localhost";
 $usuario = "root";
 $pass = "";
 $base = "zentryx";
-
+$nombre = $_POST["nombre"];
+$contrasenia = $_POST["contrasenia"];
 $conexion = new mysqli($servidor, $usuario, $pass);
 if ($conexion->connect_error) {
     die("No se pudo conectar a la base: " . $conexion->connect_error);
   }
- $prueba = mysqli_select_db($conexion, $base);
- $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nom_usuario='Juan' AND passwd='1234567'");
-if (mysqli_data_seek($consulta, 0) == true) {
-  echo "Bienvenido";
-} else {
-  echo "Nombre o contrasenia incorrectos";
-}
+  if (mysqli_select_db($conexion, $base) == true) {
+    $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nom_usuario={$nombre} AND passwd={$contrasenia}");
+    if (mysqli_data_seek($consulta, 0) == true) {
+      echo "Bienvenido";
+    } else {
+      echo "Nombre o contrasenia incorrectos";
+    }
+  }
