87606413d87c3d74e3fc79906cefa33f82dbac00
JuAND547 <juanbau.rod2007@gmail.com>
2025-06-04 09:20:04 -0300

    Update conexion_BD.php

--- conexion_BD/conexion_BD.php
+++ conexion_BD/conexion_BD.php
@@ -9,6 +9,7 @@ $conexion = new mysqli($servidor, $usuario, $pass);
 if ($conexion->connect_error) {
     die("No se pudo conectar a la base: " . $conexion->connect_error);
   }
+  function inicio($conexion, $base, $nombre, $contrasenia) {
   if (mysqli_select_db($conexion, $base) == true) {
     $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nom_usuario={$nombre} AND passwd={$contrasenia}");
     if (mysqli_data_seek($consulta, 0) == true) {
@@ -17,3 +18,9 @@ if ($conexion->connect_error) {
       echo "Nombre o contrasenia incorrectos";
     }
   }
+}
+
+function registro($conexion, $base, $nombre, $contrasenia): void {
+  $consulta = mysqli_query($conexion, "INSERT INTO usuarios('nom_usuario', 'passwd') VALUES('{$nombre}', '{$contrasenia}')" );
+  echo "Usuario {$nombre} fue registrado";
+}
