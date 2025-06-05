5061c67043062dd6798e24fbfb2b9d0345b93580
JuAND547 <juanbau.rod2007@gmail.com>
2025-06-05 08:29:30 -0300

    Add files via upload

--- conexion_BD/conexion_BD.php
+++ conexion_BD/conexion_BD.php
@@ -5,22 +5,22 @@ $pass = "";
 $base = "zentryx";
 $nombre = $_POST["nombre"];
 $contrasenia = $_POST["contrasenia"];
-$conexion = new mysqli($servidor, $usuario, $pass);
+echo "Nombre: {$nombre}";
+echo "Passwd: {$contrasenia}";
+$conexion = new mysqli($servidor, $usuario, $pass, $base);
 if ($conexion->connect_error) {
     die("No se pudo conectar a la base: " . $conexion->connect_error);
   }
+
   function inicio($conexion, $base, $nombre, $contrasenia) {
-  if (mysqli_select_db($conexion, $base) == true) {
     $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nom_usuario={$nombre} AND passwd={$contrasenia}");
     if (mysqli_data_seek($consulta, 0) == true) {
       echo "Bienvenido";
     } else {
       echo "Nombre o contrasenia incorrectos";
     }
-  }
 }
-
-function registro($conexion, $base, $nombre, $contrasenia): void {
-  $consulta = mysqli_query($conexion, "INSERT INTO usuarios('nom_usuario', 'passwd') VALUES('{$nombre}', '{$contrasenia}')" );
+ function registro($conexion, $nombre, $contrasenia) {
+  $consulta = mysqli_query($conexion, "INSERT INTO `usuarios` (`nom_usuario`, `passwd`) VALUES ('pedro', 'pedro');" );
   echo "Usuario {$nombre} fue registrado";
-}
+ }
