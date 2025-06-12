40a0a22cad17e5e9686f43ff2d383734509d5bd9
JuAND547 <juanbau.rod2007@gmail.com>
2025-06-12 08:50:37 -0300

    Add files via upload

--- navbar/conexion_BD.php
+++ navbar/conexion_BD.php
@@ -1,5 +1,5 @@
 <?php
-include("login.html");
+header("Location: login.html");
 $servidor = "localhost";
 $usuario = "root";
 $pass = "";
@@ -7,8 +7,6 @@ $base = "zentryx";
 $nombre = $_POST["nombre"];
 $contrasenia = $_POST["contrasenia"];
 $ini = $_POST["ini"];
-$documento = $DOCUMENT_ROOT. "inicio.html";
-$doc = new DOMDocument();
 echo "Nombre: {$nombre} ";
 echo "Passwd: {$contrasenia}<br>";
 $conexion = new mysqli($servidor, $usuario, $pass, $base);
@@ -16,13 +14,9 @@ if ($conexion->connect_error) {
     die("No se pudo conectar a la base: " . $conexion->connect_error);
   }
 
-  function inicio($conexion, $nombre, $contrasenia) {
+  function inicio($conexion,$nombre, $contrasenia) {
     $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nom_usuario='{$nombre}' AND passwd='{$contrasenia}'");
-    if (mysqli_data_seek($consulta, 0) == true) {
-      echo "Bienvenido, usuario";
-    } else {
-      echo "Nombre o contrasenia incorrectos";
-    }
+    return mysqli_data_seek($consulta, 0) == true;
 }
  function registro($conexion, $nombre, $contrasenia) {
   $consulta = mysqli_query($conexion, "INSERT INTO `usuarios` (`nom_usuario`, `passwd`) VALUES ('{$nombre}', '{$contrasenia}');" );
@@ -30,8 +24,10 @@ if ($conexion->connect_error) {
  }
 if ($ini == 1) {
   inicio($conexion, $nombre, $contrasenia);
-  $doc->loadHTML("inicio.html");
+  if (inicio($conexion,$nombre, $contrasenia) == true) {
+    echo "hola";
+    header("Location: pagina-principal/Inicio.html#inicio");
+  }
 } else {
   registro($conexion, $nombre, $contrasenia);
-  $doc->loadHTML("inicio.html");
 }
