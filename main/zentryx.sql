84ac340e88c83c19ea987308ce99d937190e06d3
Nach0A <nacho2006alonso@gmail.com>
2025-07-11 22:42:31 -0300

    agregando create database y use zentryx a el sql

--- main/zentryx.sql
+++ main/zentryx.sql
@@ -7,6 +7,9 @@
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
+create database if not exists `zentryx`;
+USE `zentryx`;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
