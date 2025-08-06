7e66b4f96ff51fad8fb6d92533ff9bb64adfc9d2
JuAND547 <juanbau.rod2007@gmail.com>
2025-08-06 08:30:57 -0300

    Implementando correos

--- main_final/pagina-principal/correos.php
+++ main_final/pagina-principal/correos.php
@@ -2,14 +2,14 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../phpmailer/vendor/autoload.php';
+require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com'; // Usamos el SMTP de gmail
     $mail->SMTPAuth = true;
     $mail->Username = 'zentryx.correos@gmail.com';
-    $mail->Password = 'zentryxcorreos2025';
+    $mail->Password = 'xsbp enuy wjcm muee';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
@@ -18,6 +18,8 @@ try {
     $mail->isHTML(true);
         $mail->Subject = 'Correo de prueba';
         $mail->Body    = 'Este es un correo de prueba enviado desde PHP usando PHPMailer.';
+        $mail->send();
+    echo 'Correo enviado correctamente';
 } catch (Exception $e) {
     echo "Error al enviar el correo: {$mail->ErrorInfo}";
 }
