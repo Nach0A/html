d93f2a53de2325017be1a76f944122e7c71e73d0
JuAND547 <juanbau.rod2007@gmail.com>
2025-09-15 08:43:33 -0300

    Exportando base de datos

--- main_final/pagina-principal/verifica.php
+++ main_final/pagina-principal/verifica.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "Conexion_BD.php";
+require_once "Conexion_BD.php"; 
 
 // Instancia de DB
 $db = new conexion_BD();
--- zentryx.sql
+++ zentryx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2025 at 03:27 AM
+-- Generation Time: Sep 15, 2025 at 01:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,8 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `administra` (
+  `id_admin` int(100) NOT NULL,
   `id_usuario` int(100) NOT NULL,
-  `id_grupo` int(100) NOT NULL
+  `id_grupo` int(100) NOT NULL,
+  `gmail_admin` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -39,7 +41,7 @@ CREATE TABLE `administra` (
 --
 
 CREATE TABLE `administrador` (
-  `id_admin` int(100) NOT NULL,
+  `id_admin` int(11) NOT NULL,
   `passwd_admin` varchar(64) NOT NULL,
   `calle` varchar(64) NOT NULL,
   `num_calle` varchar(64) NOT NULL,
@@ -118,8 +120,18 @@ CREATE TABLE `usuarios` (
   `id_usuario` int(100) NOT NULL,
   `nom_usuario` varchar(64) NOT NULL,
   `passwd` varchar(64) NOT NULL,
-  `gmail_usuario` varchar(64) NOT NULL
+  `gmail_usuario` varchar(64) NOT NULL,
+  `imagen_perfil` varchar(256) DEFAULT 'usuario.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nom_usuario`, `passwd`, `gmail_usuario`, `imagen_perfil`) VALUES
+(20, 'juan', 'ed08c290d7e22f7bb324b15cbadce35b0b348564fd2d5f95752388d86d71bcca', 'c21de19190f725e92595482548338a79f86e108285678731010989a866684b4f', 'usuario.png'),
+(22, '111', '5477957537', '1234314', 'usuario.png'),
+(24, 'papu', '72beaafbd6684b693da9e80c39f5f0244a8042f6371a46d95e69b05ba638f198', 'papu@gmail.com', 'usuario.png');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +141,8 @@ CREATE TABLE `usuarios` (
 -- Indexes for table `administra`
 --
 ALTER TABLE `administra`
-  ADD PRIMARY KEY (`id_usuario`,`id_grupo`),
+  ADD PRIMARY KEY (`id_admin`,`id_usuario`,`id_grupo`,`gmail_admin`),
+  ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_grupo` (`id_grupo`);
 
 --
@@ -182,12 +195,6 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT for table `administrador`
---
-ALTER TABLE `administrador`
-  MODIFY `id_admin` int(100) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `administrador_tel`
 --
 ALTER TABLE `administrador_tel`
@@ -215,7 +222,7 @@ ALTER TABLE `juego`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
@@ -225,8 +232,9 @@ ALTER TABLE `usuarios`
 -- Constraints for table `administra`
 --
 ALTER TABLE `administra`
-  ADD CONSTRAINT `administra_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  ADD CONSTRAINT `administra_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`);
+  ADD CONSTRAINT `administra_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `administrador` (`id_admin`),
+  ADD CONSTRAINT `administra_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `administra_ibfk_3` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`);
 
 --
 -- Constraints for table `administrador_tel`
@@ -258,5 +266,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-ALTER TABLE usuarios ADD COLUMN imagen_perfil VARCHAR(256) DEFAULT 'usuario.png';
