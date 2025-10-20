4fade30c7478a958ee2e04fd046842e825b04d37
Nach0A <nacho2006alonso@gmail.com>
2025-10-20 14:28:40 -0300

    .

--- main_final/buscaminas/buscaminas.php
+++ main_final/buscaminas/buscaminas.php
@@ -92,7 +92,7 @@ $user = $_SESSION['usuario'];
 
     <!-- Buscaminas -->
     <header>
-        <h1>Buscaminas</h1>
+        <h1 style="color: green;">Buscaminas</h1>
         <div id="header-original-scoreboard">
             <span id="mine-count">000</span>
             <button id="reset-button">Reiniciar</button>
