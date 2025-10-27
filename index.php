c68b9497785767b96e173f4abad1daffcbe3489a
Nach0A <nacho2006alonso@gmail.com>
2025-10-27 18:30:14 -0300

    cambiando archivo de redireccionamiento para aws

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // Redirige a la página principal dentro de la carpeta "pagina_principal"
-header("Location: main_final/pagina_principal/index.php");
+header("Location: main_final/pagina_principal/Inicio.php");
 exit();
