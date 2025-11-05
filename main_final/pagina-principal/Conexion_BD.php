0597e0f0cead2e189b881a6c9c2fe922c43b52bc
Nach0A <nacho2006alonso@gmail.com>
2025-11-05 09:14:17 -0300

    ahora creo que si

--- main_final/pagina-principal/Conexion_BD.php
+++ main_final/pagina-principal/Conexion_BD.php
@@ -18,9 +18,10 @@ class conexion_BD
     public function __construct()
     {
         $this->servidor = getenv('DB_HOST') ?: 'localhost';
-        $this->usuario  = getenv('DB_USER') ?: 'root';
+        $this->usuario  = getenv('DB_USER') ?: 'zentryx_user';
         $this->pass     = getenv('DB_PASSWORD') ?: '1';
         $this->base     = getenv('DB_NAME') ?: 'zentryx';
+
 
 
         $this->nombre = $_POST["nombre"] ?? $_POST["input"] ?? null;
