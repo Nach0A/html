6b35888f71b395939264d20eee0e35119448fda5
Nach0A <nacho2006alonso@gmail.com>
2025-11-05 08:45:22 -0300

    no se

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ require_once 'Conexion_BD.php';
 // Crear instancia de conexión
 $conexion = new conexion_BD();
 
-// Probar la conexión (opcional durante desarrollo)
-$conexion->probarConexion();
-
 // Si todo funciona, redirigir a la página principal
 header("Location: main_final/pagina-principal/Inicio.php");
 exit();
